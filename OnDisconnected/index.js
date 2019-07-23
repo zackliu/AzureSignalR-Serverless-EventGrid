2109075b39e0c99d5a192509132685a98ddb3342
Chenyang Liu <chenyl@microsoft.com>
2019-07-23 17:30:25 +0800

    update

--- OnDisconnected/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, eventGridEvent) {
-    context.log(typeof eventGridEvent);
-    context.log(eventGridEvent);
-
-    return {
-        "target": "connectionCount",
-        "arguments": [ 2 ]
-    }
-};
